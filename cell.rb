d25a7eae1ce722aa128b6be582167dbf29f7c6ed
Bob Walsh <bob.walsh@47hats.com>
2013-03-30 14:30:30 -0700

    working code

--- cell.rb
+++ cell.rb
@@ -1,7 +1,13 @@
 class Cell
-  attr_accessor :state
+  attr_accessor :state, :col, :row
   
-  def initialize
+  def initialize(col=0,row=0)
+  	self.col = col
+  	self.row = row
     self.state = false
+  end
+
+  def location
+  	[col,row]
   end
 end
